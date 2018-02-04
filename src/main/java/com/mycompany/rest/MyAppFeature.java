fed11e25b600d88f114a50f098f2af2a9c3928ba
John D. Ament <johndament@apache.org>
2018-02-04 11:15:20 -0500

    Base project.

---
+++ src/main/java/com/mycompany/rest/MyAppFeature.java
@@ -0,0 +1,35 @@
+package com.mycompany.rest;
+
+import org.apache.cxf.cdi.extension.JAXRSServerFactoryCustomizationExtension;
+import org.apache.cxf.interceptor.LoggingOutInterceptor;
+import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
+import org.apache.cxf.jaxrs.provider.StreamingResponseProvider;
+import org.apache.cxf.jaxrs.reactor.client.ReactorInvokerProvider;
+import org.apache.cxf.jaxrs.reactor.server.ReactorInvoker;
+import org.apache.johnzon.jaxrs.JsrProvider;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.json.JsonObject;
+import javax.ws.rs.core.Feature;
+import javax.ws.rs.core.FeatureContext;
+import java.util.Collections;
+
+@ApplicationScoped
+public class MyAppFeature implements Feature, JAXRSServerFactoryCustomizationExtension {
+    public boolean configure(FeatureContext featureContext) {
+        featureContext.register(JsrProvider.class);
+        featureContext.register(ReactorInvokerProvider.class);
+        return true;
+    }
+
+    @Override
+    public void customize(JAXRSServerFactoryBean jaxrsServerFactoryBean) {
+        ReactorInvoker invoker = new ReactorInvoker();
+        invoker.setUseStreamingSubscriberIfPossible(false);
+        jaxrsServerFactoryBean.setInvoker(invoker);
+        StreamingResponseProvider<JsonObject> streamProvider = new StreamingResponseProvider<>();
+        streamProvider.setProduceMediaTypes(Collections.singletonList("application/json"));
+        jaxrsServerFactoryBean.setProvider(streamProvider);
+        jaxrsServerFactoryBean.getOutInterceptors().add(new LoggingOutInterceptor());
+    }
+}
---
+++ src/main/java/com/mycompany/rest/RestController.java
@@ -0,0 +1,32 @@
+package com.mycompany.rest;
+
+import org.apache.cxf.jaxrs.reactivestreams.server.JsonStreamingAsyncSubscriber;
+import org.apache.cxf.jaxrs.reactor.client.ReactorInvoker;
+
+import javax.enterprise.context.RequestScoped;
+import javax.json.JsonObject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
+import javax.ws.rs.core.MediaType;
+
+@RequestScoped
+@Path("/")
+public class RestController {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public void doGet(@Suspended AsyncResponse asyncResponse) {
+        ClientBuilder.newClient()
+                .register(MyAppFeature.class)
+                .target("https://postman-echo.com/get")
+                .queryParam("arg1", "arg1")
+                .queryParam("arg2", "arg2")
+                .request(MediaType.APPLICATION_JSON_TYPE)
+                .rx(ReactorInvoker.class)
+                .get(JsonObject.class)
+                .subscribe(new JsonStreamingAsyncSubscriber<>(asyncResponse));
+    }
+}
